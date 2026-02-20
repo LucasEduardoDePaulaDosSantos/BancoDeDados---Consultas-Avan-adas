c84f9087272737033f1031b5490c67cb2e7d71a4
LucasEduardoDePaulaDosSantos <paula.santos.lucas1811@escola.pr.gov.br>
2026-02-20 18:20:17 -0300

    Consultas Avançadas

---
+++ Consultas Avançadas.sql
@@ -0,0 +1,25 @@
+use sucos_vendas;
+  
+SELECT * FROM tabela_de_clientes;
+  
+SELECT CPF, NOME FROM tabela_de_clientes;
+    
+SELECT * FROM tabela_de_clientes WHERE cpf = '1471156710';
+
+SELECT * FROM tabela_de_clientes WHERE nome = 'Marcelo Mattos ';
+
+SELECT * FROM tabela_de_produtos;
+SELECT * FROM tabela_de_produtos WHERE preco_de_lista > 8.41;
+
+SELECT * FROM tabela_de_clientes WHERE preco_de_lista 
+BETWEEN 8.40 AND 8.42; 
+
+SELECT * FROM tabela_de_produtos WHERE sabor = 'Manga' OR tamanho = '350 ml';
+
+SELECT * FROM tabela_de_produtos WHERE sabor = 'Manga' AND tamanho = '350 ml';
+
+SELECT * FROM tabela_de_produtos WHERE
+NOT (sabor = 'Manga' AND tamanho = '350 ml');
+
+SELECT * FROM tabela_de_produtos WHERE
+NOT (sabor = 'Manga' OR tamanho = '350 ml');
